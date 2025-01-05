03dc45dc7291c698e6cab8696d9c5102a0a8b333
abnerjosefelixbarbosa <abnerbarbosa95@gmail.com>
2025-01-05 16:08:27 -0300

    commited by Abner

---
+++ ze-delivery-spring-boot-back-end-java/src/main/java/com/org/ze_delivery_spring_boot_back_end_java/ZeDeliverySpringBootBackEndJavaApplication.java
@@ -0,0 +1,13 @@
+package com.org.ze_delivery_spring_boot_back_end_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZeDeliverySpringBootBackEndJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZeDeliverySpringBootBackEndJavaApplication.class, args);
+	}
+
+}
